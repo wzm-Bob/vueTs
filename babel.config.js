97b3600cbfd569b146b314982a6bceab35b6e11b
wangzm <1341591227@qq.com>
2018-10-22 19:13:55 +0800

    vuecli3.0配置提交

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    presets: [
+        '@vue/babel-preset-app'
+    ]
+    /* 按需加载插件先注释掉噢
+    plugins: [
+        ["import", {
+            "libraryName": "vant",
+            "libraryDirectory": "es",
+            "style": true
+        }]
+    ] */
+}
--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,8 @@ declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
 }
+/* 
+这是定义文件，为了让ts代码识别 .vue 文件
+在ts代码中导入vue代码包，需要写成 import xxx form xxx.vue ，
+需要带上后缀 vue
+ */
